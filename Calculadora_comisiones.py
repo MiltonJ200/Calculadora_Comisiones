51134320fcc05dd7e1c712683d6abcab64302cfe
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-16 08:20:00 -0500

    Calculadora de comisiones
    Calculadora realizada con el conocimiento adquirido en el curso de Python Total

---
+++ Calculadora_comisiones.py
@@ -0,0 +1,7 @@
+nombre = input("Cual es tu nombre? ")
+ventas = round(float((input("Cuanto has vendido este mes? "))), 2)
+
+comisiones = ventas * 13 / 100
+comisiones_redondeadas = round(comisiones, 2)
+
+print(f"Hola {nombre} tus ventas fueron {ventas} por lo que tu comision es de {comisiones_redondeadas}")
